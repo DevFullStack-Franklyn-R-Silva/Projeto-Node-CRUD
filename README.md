c57072bd32cf109379f84e8ffc80f446882f822e
Franklyn R <franklyn1998@live.com>
2022-10-04 16:29:30 -0300

    Projeto: API com Express #02

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Projeto-Node
-Projeto Node do Curso Web Moderno
+# Projeto Node do Curso Web Moderno
+
+## O que foi instalado no projeto Node
+
+### Projeto: API com Express #01
+```node
+npm init -y
+npm i --save express@4.16.2 -E
+npm i --save-dev nodemon@1.14.11 -E
+```
--- src/servidor.js
+++ src/servidor.js
@@ -2,9 +2,22 @@ const porta = 3004;
 
 const express = require("express");
 const app = express();
+const bancoDeDados = require("./bancoDeDados");
 
 app.get("/produtos", (req, res, next) => {
-    res.send({ nome: "Notebook", preco: 123.45 }); // Converter para JSON
+    res.send(bancoDeDados.getProdutos);
+});
+
+app.get("/produtos/:id", (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post("/produtos/:id", (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco,
+    });
+    res.send(produto); // JSON
 });
 
 app.listen(porta, () => {
